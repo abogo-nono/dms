9bf4c0cc1eb734d382b7d65ee7ed4e24af331c6f
Abo <abogonono1@gmail.com>
2025-06-07 10:09:57 +0100

    Add Ingredient model with attributes and serialization method

---
+++ app/models/ingredient.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    food_ingredients = db.relationship('FoodIngredient', back_populates='ingredient', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+    
