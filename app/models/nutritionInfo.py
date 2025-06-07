5f11e4739664575fe58af7b08741396b6f70b9cc
Abo <abogonono1@gmail.com>
2025-06-07 10:10:42 +0100

    Add NutritionInfo model with attributes and serialization method

---
+++ app/models/nutritionInfo.py
@@ -0,0 +1,23 @@
+from app import db
+from sqlalchemy.orm import relationship
+
+
+class NutritionInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    food_id = db.Column(db.Integer, db.ForeignKey('food.id'), nullable=False)
+    calories = db.Column(db.Float, nullable=False)
+    protein = db.Column(db.Float, nullable=False)
+    fat = db.Column(db.Float, nullable=False)
+    carbohydrates = db.Column(db.Float, nullable=False)
+
+    food = relationship('Food', back_populates='nutrition_info')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'food_id': self.food_id,
+            'calories': self.calories,
+            'protein': self.protein,
+            'fat': self.fat,
+            'carbohydrates': self.carbohydrates
+        }
