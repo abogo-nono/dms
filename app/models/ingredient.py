6c06191f4247b95112b94b8e7e03ce2754bc1268
Abo <abogonono1@gmail.com>
2025-06-17 03:55:32 +0100

    Refactor Ingredient model for consistent formatting and improved readability

--- app/models/ingredient.py
+++ app/models/ingredient.py
@@ -4,11 +4,12 @@ from app import db
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-    food_ingredients = db.relationship('FoodIngredient', back_populates='ingredient', cascade='all, delete-orphan')
+    food_ingredients = db.relationship(
+        "FoodIngredient", back_populates="ingredient", cascade="all, delete-orphan"
+    )
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'name': self.name,
+            "id": self.id,
+            "name": self.name,
         }
-    
