264d8ac62ee59af89381d62b8a43ed6745377fe5
Abo <abogonono1@gmail.com>
2025-06-17 03:56:38 +0100

    Refactor Person resource for consistent formatting and improved readability

--- app/resources/person.py
+++ app/resources/person.py
@@ -1,50 +1,53 @@
 from typing import List
-from flask_restful import Resource, abort, reqparse
+
 from flasgger import swag_from
-from app.models.person import Person
+from flask_restful import Resource, abort, reqparse
+
 from app import db
+from app.models.person import Person
 
 parser = reqparse.RequestParser()
-parser.add_argument('name', type=str, required=True)
-parser.add_argument('gender', type=str, required=True)
-parser.add_argument('age', type=int, required=True)
+parser.add_argument("name", type=str, required=True)
+parser.add_argument("gender", type=str, required=True)
+parser.add_argument("age", type=int, required=True)
+
 
 class PersonResource(Resource):
-    @swag_from('../docs/person.yml', endpoint='people')
+    @swag_from("../docs/person.yml", endpoint="people")
     def get(self, person_id):
-        person: Person|None = Person.query.get_or_404(person_id, 'Person not found')
-        return {'person': person.to_dict()}, 200
+        person: Person | None = Person.query.get_or_404(person_id, "Person not found")
+        return {"person": person.to_dict()}, 200
 
-    @swag_from('../docs/person.yml', endpoint='people')
+    @swag_from("../docs/person.yml", endpoint="people")
     def put(self, person_id):
-        person: Person = Person.query.get_or_404(person_id, 'Person not found')
+        person: Person = Person.query.get_or_404(person_id, "Person not found")
         args = parser.parse_args()
-        person.name = args['name']
-        person.gender = args['gender']
-        person.age = args['age']
+        person.name = args["name"]
+        person.gender = args["gender"]
+        person.age = args["age"]
         db.session.commit()
-        return {'person': person.to_dict()}, 201
+        return {"person": person.to_dict()}, 201
 
-    @swag_from('../docs/person.yml', endpoint='people')
+    @swag_from("../docs/person.yml", endpoint="people")
     def delete(self, person_id):
-        person: Person = Person.query.get_or_404(person_id, 'Person not found')
+        person: Person = Person.query.get_or_404(person_id, "Person not found")
         db.session.delete(person)
         db.session.commit()
-        return {'message': f'`{person.name}` has been deleted!'}, 204
+        return {"message": f"`{person.name}` has been deleted!"}, 204
+
 
 class PersonListResource(Resource):
-    @swag_from('../docs/person.yml', endpoint='people_list')
+    @swag_from("../docs/person.yml", endpoint="people_list")
     def get(self):
         people: List[Person] = Person.query.all()
-        return {'people': [person.to_dict() for person in people]}, 200
+        return {"people": [person.to_dict() for person in people]}, 200
 
-    @swag_from('../docs/person.yml', endpoint='people_list')
+    @swag_from("../docs/person.yml", endpoint="people_list")
     def post(self):
         args = parser.parse_args()
-        if Person.query.filter_by(name=args['name']).first():
-            abort(400, description='This name is already exists!')
+        if Person.query.filter_by(name=args["name"]).first():
+            abort(400, description="This name is already exists!")
         person = Person(**args)
         db.session.add(person)
         db.session.commit()
-        return {'person': person.to_dict()}, 201
-
+        return {"person": person.to_dict()}, 201
