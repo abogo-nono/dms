8dc69ea4b5f9011c0d2e815e8192c46206937fa9
Abo <abogonono1@gmail.com>
2025-06-17 03:54:58 +0100

    Refactor Food model for improved formatting and readability

--- app/models/food.py
+++ app/models/food.py
@@ -1,4 +1,5 @@
-from app  import db
+from app import db
+
 
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,16 +7,22 @@ class Food(db.Model):
     description = db.Column(db.String(255), nullable=True)
     instructions = db.Column(db.Text, nullable=True)
     image_url = db.Column(db.String(255), nullable=True)
-    ingredients = db.relationship('FoodIngredient', back_populates='food', cascade='all, delete-orphan')
-    nutrition_info = db.relationship('NutritionInfo', back_populates='food', uselist=False)
+    ingredients = db.relationship(
+        "FoodIngredient", back_populates="food", cascade="all, delete-orphan"
+    )
+    nutrition_info = db.relationship(
+        "NutritionInfo", back_populates="food", uselist=False
+    )
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'instructions': self.instructions,
-            'image_url': self.image_url,
-            'ingredients': [fi.to_dict() for fi in self.ingredients],
-            'nutrition_info': self.nutrition_info.to_dict() if self.nutrition_info else None
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "instructions": self.instructions,
+            "image_url": self.image_url,
+            "ingredients": [fi.to_dict() for fi in self.ingredients],
+            "nutrition_info": (
+                self.nutrition_info.to_dict() if self.nutrition_info else None
+            ),
         }
